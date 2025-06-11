7d2f7d0337186b63a384b9064d0de924680c630c
Herman-Gathege <172432295+Herman-Gathege@users.noreply.github.com>
2025-06-11 14:40:31 +0300

    added projects to site

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,20 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+
+ document.addEventListener("DOMContentLoaded", function () {
+    const loadMoreBtn = document.getElementById("loadMoreBtn");
+    loadMoreBtn.addEventListener("click", () => {
+      const hiddenItems = document.querySelectorAll(".portfolio-item.hidden");
+
+      hiddenItems.forEach((item, index) => {
+        setTimeout(() => {
+          item.classList.remove("hidden");
+          item.classList.add("fade-in");
+        }, index * 100); // staggered reveal
+      });
+
+      loadMoreBtn.style.display = "none"; // hide button after loading
+    });
+  });
